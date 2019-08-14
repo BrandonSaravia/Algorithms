4df7a1b6d4001e8195e58e029c2a125b8c430b06
BrandonSaravia <BPSaravia@gmail.com>
2019-08-14 19:47:33 -0400

    finishing up first problem

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,23 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  if type(prices) != list:
+    return "Enter a list of stock prices"
+
+  current_min_price_so_far = 0
+  max_profit_so_far = 0
+  maximumIndex = 0
+
+  for i in range(prices):
+
+    if max_profit_so_far <= prices[i]:
+      max_profit_so_far = prices[i]
+      maximumIndex = i
+
+    if i <= maximumIndex and current_min_price_so_far >= prices[i]:
+      current_min_price_so_far = prices[i]
+
+  profit = max_profit_so_far - current_min_price_so_far
 
 
 if __name__ == '__main__':
